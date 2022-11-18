8707e189ddefdf1bdfa250b6b4495b15fac28c2f
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-11-18 07:54:31 +0300

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -5,11 +5,3 @@
 
 
 
-from aiohttp import web
-from .route import routes
-
-
-async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
